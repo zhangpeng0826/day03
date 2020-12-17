722cc40cfc38be4dfd3022c36a5b7929a344020a
zhangpeng0826 <931649221@qq.com>
2020-12-17 21:42:55 +0800

    第3遍第2次提交

--- app3/src/main/java/com/example/app3/MainActivity.java
+++ app3/src/main/java/com/example/app3/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.app3;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv = findViewById(R.id.tv);
     }
 }
