727c892eb15a506746d44264f99592af45c9e30e
zhangpeng0826 <931649221@qq.com>
2020-12-17 21:11:43 +0800

    第2遍第7次提交

--- app2/src/main/java/com/example/app2/urils/URL.java
+++ app2/src/main/java/com/example/app2/urils/URL.java
@@ -1,5 +1,6 @@
 package com.example.app2.urils;
 
 public class URL {
-
+        public static String Url = "http://www.qubaobei.com/ios/cf/";
+        public static String FoodUrl = "dish_list.php?stage_id=1&limit=20&page=1";
 }
