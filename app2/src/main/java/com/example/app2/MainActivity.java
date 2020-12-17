7dafaf2c75e74e4e1cd6913a9aed7d454dbf40cd
zhangpeng0826 <931649221@qq.com>
2020-12-17 21:31:14 +0800

    第2遍第11次提交

--- app2/src/main/java/com/example/app2/MainActivity.java
+++ app2/src/main/java/com/example/app2/MainActivity.java
@@ -5,25 +5,44 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.app2.base.BaseActivity;
 import com.example.app2.base.BaseView;
+import com.example.app2.bean.Bean;
+import com.example.app2.contract.ConTract;
+import com.example.app2.presenter.PresenterImp;
 
-public class MainActivity extends AppCompatActivity implements BaseView {
+public class MainActivity extends BaseActivity<PresenterImp> implements ConTract.View {
 
     private TextView tv;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initView();
-        initData();
+    public void initData() {
+        getpresenter().getpresenter();
     }
 
-    private void initData() {
-
-    }
-
-    private void initView() {
+    public void initView() {
         tv = findViewById(R.id.tv);
+    }
+
+    @Override
+    protected PresenterImp getpresenter() {
+        return new PresenterImp();
+    }
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+
+
+    @Override
+    public void OnOk(Bean bean) {
+        tv.setText(bean.getData().get(0).getTitle());
+    }
+
+    @Override
+    public void OnNo(String error) {
+
     }
 }
---
+++ app2/src/main/java/com/example/app2/presenter/PresenterImp.java
@@ -0,0 +1,31 @@
+package com.example.app2.presenter;
+
+import com.example.app2.MainActivity;
+import com.example.app2.base.BaseActivity;
+import com.example.app2.base.BasePresenter;
+import com.example.app2.bean.Bean;
+import com.example.app2.callback.TvCallBack;
+import com.example.app2.contract.ConTract;
+import com.example.app2.model.ModelImp;
+import com.example.app2.urils.URL;
+
+public class PresenterImp extends BasePresenter implements ConTract.Presenter {
+    private ModelImp modelImp;
+    public PresenterImp() {
+        modelImp = new ModelImp(this);
+    }
+
+    @Override
+    public void getpresenter() {
+        modelImp.getModel(URL.FoodUrl, new TvCallBack<Bean>() {
+            @Override
+            public void OnOk(Bean bean) {
+            }
+
+            @Override
+            public void OnNo(String error) {
+
+            }
+        });
+    }
+}
